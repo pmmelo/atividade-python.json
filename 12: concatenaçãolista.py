11a7968f753db7d356dfe1ac0269116b7a6f7244
Pedro Henrique <pemedeiros.al@gmail.com>
2025-08-12 16:51:14 -0300

    Create 12: concatenaçãolista.py

---
+++ 12: concatenaçãolista.py
@@ -0,0 +1,8 @@
+def concatenar_listas(lista1, lista2):
+    return lista1 + lista2
+
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+
+resultado = concatenar_listas(natureza, tecnologia)
+print(resultado)
